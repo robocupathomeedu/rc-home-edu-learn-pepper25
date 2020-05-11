d37b79cb7d79bf81600416621cd213ffd2fbc423
jeffreyttc <i@jeffreytan.org>
2020-05-12 00:33:26 +0800

    added visual perception examples

---
+++ cn/01 示例程序/02 视觉部分/人脸学习功能/translations/translation_en_US.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说学习失败</name>
+        <message>
+            <source>学习失败，再试一次</source>
+            <comment>Text</comment>
+            <translation type="obsolete">学习失败，再试一次</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>学习失败</source>
+            <comment>Text</comment>
+            <translation type="unfinished">学习失败</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说学习成功</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>学习成功</source>
+            <comment>Text</comment>
+            <translation type="unfinished">学习成功</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说开始</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>现在开始学习人脸</source>
+            <comment>Text</comment>
+            <translation type="unfinished">现在开始学习人脸</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_识别人脸/_说开始识别人脸</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>开始识别人脸</source>
+            <comment>Text</comment>
+            <translation type="unfinished">开始识别人脸</translation>
+        </message>
+    </context>
+</TS>
---
+++ cn/01 示例程序/02 视觉部分/人脸学习功能/translations/translation_zh_CN.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说学习失败</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>学习失败</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说学习成功</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>学习成功</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说开始</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>现在开始学习人脸</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_识别人脸/_说开始识别人脸</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>开始识别人脸</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
