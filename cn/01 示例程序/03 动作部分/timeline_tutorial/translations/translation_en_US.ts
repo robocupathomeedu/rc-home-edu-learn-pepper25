6b4f9ba28ca0b7b39b227bdd1a9308ce45c15fce
jeffreyttc <i@jeffreytan.org>
2020-05-19 12:25:13 +0800

    added body gesture examples

---
+++ cn/01 示例程序/03 动作部分/timeline_tutorial/translations/translation_en_US.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>我在动</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我在动</translation>
+        </message>
+        <message>
+            <source>我正在动</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我正在动</translation>
+        </message>
+        <message>
+            <source>我在关键框架1</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我在关键框架1</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer2/keyframe1/Say</name>
+        <message>
+            <source>我在关键框架2</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我在关键框架2</translation>
+        </message>
+        <message>
+            <source>我在关键框架1</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我在关键框架1</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>我在动</source>
+            <comment>Text</comment>
+            <translation type="unfinished">我在动</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer2/keyframe17/Say</name>
+        <message>
+            <source>我在关键框架2</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我在关键框架2</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer2/keyframe2/Say</name>
+        <message>
+            <source>我在关键框架2</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我在关键框架2</translation>
+        </message>
+    </context>
+</TS>
---
+++ cn/01 示例程序/03 动作部分/timeline_tutorial/translations/translation_zh_CN.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">你好</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer2/keyframe1/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>我在动</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
