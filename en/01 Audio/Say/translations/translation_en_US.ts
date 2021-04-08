572bb932874e8f59c9345ba9a4b49eff8120b946
Jeffrey Tan <i@jeffreytan.org>
2021-04-08 17:43:31 +0800

    Added audio examples in English

---
+++ en/01 Audio/Say/translations/translation_en_US.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>我在使用上下文模式</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我在使用上下文模式</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am using context mode</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am using context mode</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Modify Say Parameters/Say</name>
+        <message>
+            <source>我还可以改变语调来说话</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我还可以改变语调来说话</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I can also change the tone of voice to speak</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I can also change the tone of voice to speak</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Modify Say Parameters/Say (1)</name>
+        <message>
+            <source>我还可以改变语速来说话</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我还可以改变语速来说话</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I can also change the rate of speech to speak</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I can also change the rate of speech to speak</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say in English/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, my name is Pepper</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, my name is Pepper</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Say in English/Say</name>
+        <message>
+            <source>Hello, my name is Pepper</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, my name is Pepper</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_修改说话参数/Say</name>
+        <message>
+            <source>我还可以改变语调来说话</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我还可以改变语调来说话</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_修改说话参数/Say (1)</name>
+        <message>
+            <source>我还可以改变语速来说话</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我还可以改变语速来说话</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_说英文/Say</name>
+        <message>
+            <source>Hello, my name is Pepper</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, my name is Pepper</translation>
+        </message>
+    </context>
+</TS>
---
+++ en/01 Audio/Say/translations/translation_zh_CN.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am using context mode</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Modify Say Parameters/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I can also change the tone of voice to speak</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Modify Say Parameters/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I can also change the rate of speech to speak</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say in English/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, my name is Pepper</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
