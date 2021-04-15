ae021fbb6d013e1b6790ff57666d303fdbde3c77
Jeffrey Tan <i@jeffreytan.org>
2021-04-15 17:34:28 +0800

    Added vision examples in English

---
+++ en/02 Vision/Face_Learning/translations/translation_en_US.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Learning/_Fail to learn</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>failure to learn</source>
+            <comment>Text</comment>
+            <translation type="unfinished">failure to learn</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Learning/_Say to start</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Learning face now</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Learning face now</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>learning face now</source>
+            <comment>Text</comment>
+            <translation type="unfinished">learning face now</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Learning/_Success to learn</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>learn successfully</source>
+            <comment>Text</comment>
+            <translation type="unfinished">learn successfully</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Rec/_Say to recognize face</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Begin to recognize face</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Begin to recognize face</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说学习失败</name>
+        <message>
+            <source>学习失败，再试一次</source>
+            <comment>Text</comment>
+            <translation type="obsolete">学习失败，再试一次</translation>
+        </message>
+        <message>
+            <source>学习失败</source>
+            <comment>Text</comment>
+            <translation type="obsolete">学习失败</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说学习成功</name>
+        <message>
+            <source>学习成功</source>
+            <comment>Text</comment>
+            <translation type="obsolete">学习成功</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_学习人脸/_说开始</name>
+        <message>
+            <source>现在开始学习人脸</source>
+            <comment>Text</comment>
+            <translation type="obsolete">现在开始学习人脸</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_识别人脸/_说开始识别人脸</name>
+        <message>
+            <source>开始识别人脸</source>
+            <comment>Text</comment>
+            <translation type="obsolete">开始识别人脸</translation>
+        </message>
+    </context>
+</TS>
---
+++ en/02 Vision/Face_Learning/translations/translation_zh_CN.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Learning/_Fail to learn</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>failure to learn</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Learning/_Say to start</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Learning face now</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>learning face now</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Learning/_Success to learn</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>learn successfully</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Rec/_Say to recognize face</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Begin to recognize face</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
